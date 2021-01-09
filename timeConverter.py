2a05a54f11ff7446b0f1aa88e6cd61d271ea49df
gleungg <gordonleungg@gmail.com>
2021-01-09 14:43:40 -0800

    Create time zone conversion tool

--- timeConverter.py
+++ timeConverter.py
@@ -1,6 +1,16 @@
 from datetime import datetime
+from pytz import timezone
 
-
-# pass inialize with time along with a time zone then store it in UMT  
 class TimeConverter: 
-    def __init__(self, ):
+    def __init__(self, datetimeObj, timeZone = None):
+        if not datetimeObj.tzinfo:
+            try:
+                datetimeObj.replace(tzinfo=timezone(timeZone))
+            except:
+                print("No time zone information could be found")
+
+        self.timeInUTC = self.getTimeInUTC(datetimeObj)
+        
+
+    def getTimeInUTC(self, datetimeObj):
+        return datetimeObj.astimezone(timezone('UTC'))
